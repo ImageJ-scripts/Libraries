9e36cec3635260d6830e11eb289e0f4b20281d0b
Igor Bonacossa Pereira <uqibonac@uq.edu.au>
2020-01-06 13:07:14 +1000

    Updating constants

--- IBPlib/ij/Constants.py
+++ IBPlib/ij/Constants.py
@@ -10,10 +10,5 @@ COLORS = (
 	"Yellow"
 )
 LIBPREFSKEY = "IBPlib"
-<<<<<<< HEAD
-USER_STORAGE = "/afm01/Q1/Q1254/Igor BP/Projects/Let-805/Raw Data/Microscopies/Yokogawa Spinning Disk/"
-USER_SCRATCH = "/scratch/qbi/uqibonac/"
-=======
 __STORAGE_DIR__ = ""
 __MAGNIFICATION_DIR__ = "63x OIL"
->>>>>>> 8f6091a24d695e99b106de86e536cb2b16121159
